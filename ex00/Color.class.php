5c143fb13b4507a409cfc6d7e096faab19066c61
tmanet <tmanet@student.42.fr>
2016-04-12 14:59:57 +0200

    debut

--- ex00/Color.class.php
+++ ex00/Color.class.php
@@ -11,71 +11,56 @@ public static $verbose = false;
 
 public static function doc() {
 
-	echo "
-
-<- Color ----------------------------------------------------------------------
-The Color class handles RGB colors.
-
-An instance can be contructed from either a RGB value:
-new Color( array( 'rgb' => 12345 ) );
-
-Or from distinct red, green, blue constitutives:
-new Color( array( 'red' => 255, 'green' => 255, 'blue' => 255 ) );
-
-Red, green, blue constitutives and RGB values are converted to intergers.
-Negative or > to 255 color constitutives are left as is.
-Any other use is undefined behaviour.
-
-The class provides the following methods :
-
-Color   add( Color \$rhs );
-Adds each color constitutive and returns a new Color instance.
-
-Color   sub( Color \$rhs );
-Substracts each color constitutive and returns a new Color instance.
-
-Color   mult( \$f );
-Multiplies each color constitutive by factor \$f and returns a new Color
-instance.
----------------------------------------------------------------------- Color ->\n";
+print (file_get_contents('Color.doc.txt')."\n");
 
 }
 
+private function _color_setter($color) {
+
+return ($color % 256);
+
+}
 
 function __construct( array $kwargs ) {
 	if ( array_key_exists( 'rgb', $kwargs))
-	$this->green = 255;
+	{
+		$this->red = $this->_color_setter($kwargs['rgb'] / (256 * 256));
+		$this->green = $this->_color_setter($kwargs['rgb'] >> 8);
+		$this->blue = $this->_color_setter($kwargs['rgb']);
+	}
 	else
 	{
 
 	if ( array_key_exists( 'red', $kwargs))
-	$this->red = $kwargs['red'];
+	$this->red = $this->_color_setter($kwargs['red']);
 	if ( array_key_exists( 'green', $kwargs))
-	$this->green = $kwargs['green'];
+	$this->green = $this->_color_setter($kwargs['green']);
 	if ( array_key_exists( 'blue', $kwargs))
-	$this->blue = $kwargs['blue'];
+	$this->blue = $this->_color_setter($kwargs['blue']);
 
 }
 
 		if (self::$verbose)
 		{
-			$this->_descriptor();
-			print (" constructed.\n");
+			print ($this->_descriptor()." constructed.\n");
 		}
 }
 
 private function _descriptor() {
 
-			printf("Color( red: %3d, green: %3d, blue: %3d )", $this->red, $this->green, $this->blue);
+			return(sprintf("Color( red: %3d, green: %3d, blue: %3d )", $this->red, $this->green, $this->blue));
 
 }
 
 public function __tostring() {
 
-			$this->_descriptor();
-			print ("\n");
+			return ($this->_descriptor()."\n");
 }
 
+public function add() {
+
+
+}
 
 }
 
